14d7d3697301a269ba9ab630ed33109c5ad393b0
Masahiro Aoki <freud427@gmail.com>
2023-09-26 00:17:55 +0900

    initial commit

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -5,7 +5,7 @@ import 'dart:ui' as ui;
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter/services.dart';
-import 'package:gallery_saver/gallery_saver.dart';
+import 'package:flutter_gallery_saver/gallery_saver.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:path_provider/path_provider.dart';
 
@@ -76,61 +76,61 @@ class _MyAppState extends State<MyApp> {
   }
 
   void _takePhoto() async {
-    ImagePicker()
-        .getImage(source: ImageSource.camera)
-        .then((PickedFile recordedImage) {
-      if (recordedImage != null && recordedImage.path != null) {
-        setState(() {
-          firstButtonText = 'saving in progress...';
-        });
-        GallerySaver.saveImage(recordedImage.path, albumName: albumName)
-            .then((bool success) {
-          setState(() {
-            firstButtonText = 'image saved!';
-          });
-        });
-      }
-    });
+    // ImagePicker()
+    //     .getImage(source: ImageSource.camera)
+    //     .then((PickedFile recordedImage) {
+    //   if (recordedImage != null && recordedImage.path != null) {
+    //     setState(() {
+    //       firstButtonText = 'saving in progress...';
+    //     });
+    //     GallerySaver.saveImage(recordedImage.path, albumName: albumName)
+    //         .then((bool success) {
+    //       setState(() {
+    //         firstButtonText = 'image saved!';
+    //       });
+    //     });
+    //   }
+    // });
   }
 
   void _recordVideo() async {
-    ImagePicker()
-        .getVideo(source: ImageSource.camera)
-        .then((PickedFile recordedVideo) {
-      if (recordedVideo != null && recordedVideo.path != null) {
-        setState(() {
-          secondButtonText = 'saving in progress...';
-        });
-        GallerySaver.saveVideo(recordedVideo.path, albumName: albumName)
-            .then((bool success) {
-          setState(() {
-            secondButtonText = 'video saved!';
-          });
-        });
-      }
-    });
+    // ImagePicker()
+    //     .getVideo(source: ImageSource.camera)
+    //     .then((PickedFile recordedVideo) {
+    //   if (recordedVideo != null && recordedVideo.path != null) {
+    //     setState(() {
+    //       secondButtonText = 'saving in progress...';
+    //     });
+    //     GallerySaver.saveVideo(recordedVideo.path, albumName: albumName)
+    //         .then((bool success) {
+    //       setState(() {
+    //         secondButtonText = 'video saved!';
+    //       });
+    //     });
+    //   }
+    // });
   }
 
   // ignore: unused_element
   void _saveNetworkVideo() async {
-    String path =
-        'https://sample-videos.com/video123/mp4/720/big_buck_bunny_720p_1mb.mp4';
-    GallerySaver.saveVideo(path, albumName: albumName).then((bool success) {
-      setState(() {
-        print('Video is saved');
-      });
-    });
+    // String path =
+    //     'https://sample-videos.com/video123/mp4/720/big_buck_bunny_720p_1mb.mp4';
+    // GallerySaver.saveVideo(path, albumName: albumName).then((bool success) {
+    //   setState(() {
+    //     print('Video is saved');
+    //   });
+    // });
   }
 
   // ignore: unused_element
   void _saveNetworkImage() async {
-    String path =
-        'https://image.shutterstock.com/image-photo/montreal-canada-july-11-2019-600w-1450023539.jpg';
-    GallerySaver.saveImage(path, albumName: albumName).then((bool success) {
-      setState(() {
-        print('Image is saved');
-      });
-    });
+    // String path =
+    //     'https://image.shutterstock.com/image-photo/montreal-canada-july-11-2019-600w-1450023539.jpg';
+    // GallerySaver.saveImage(path, albumName: albumName).then((bool success) {
+    //   setState(() {
+    //     print('Image is saved');
+    //   });
+    // });
   }
 }
 
@@ -166,32 +166,32 @@ class _ScreenshotWidgetState extends State<ScreenshotWidget> {
   }
 
   Future<void> _saveScreenshot() async {
-    setState(() {
-      screenshotButtonText = 'saving in progress...';
-    });
-    try {
-      //extract bytes
-      final RenderRepaintBoundary boundary =
-          _globalKey.currentContext.findRenderObject();
-      final ui.Image image = await boundary.toImage(pixelRatio: 3.0);
-      final ByteData byteData =
-          await image.toByteData(format: ui.ImageByteFormat.png);
-      final Uint8List pngBytes = byteData.buffer.asUint8List();
+    // setState(() {
+    //   screenshotButtonText = 'saving in progress...';
+    // });
+    // try {
+    //   //extract bytes
+    //   final RenderRepaintBoundary boundary =
+    //       _globalKey.currentContext.findRenderObject();
+    //   final ui.Image image = await boundary.toImage(pixelRatio: 3.0);
+    //   final ByteData byteData =
+    //       await image.toByteData(format: ui.ImageByteFormat.png);
+    //   final Uint8List pngBytes = byteData.buffer.asUint8List();
 
-      //create file
-      final String dir = (await getApplicationDocumentsDirectory()).path;
-      final String fullPath = '$dir/${DateTime.now().millisecond}.png';
-      File capturedFile = File(fullPath);
-      await capturedFile.writeAsBytes(pngBytes);
-      print(capturedFile.path);
+    //   //create file
+    //   final String dir = (await getApplicationDocumentsDirectory()).path;
+    //   final String fullPath = '$dir/${DateTime.now().millisecond}.png';
+    //   File capturedFile = File(fullPath);
+    //   await capturedFile.writeAsBytes(pngBytes);
+    //   print(capturedFile.path);
 
-      await GallerySaver.saveImage(capturedFile.path).then((value) {
-        setState(() {
-          screenshotButtonText = 'screenshot saved!';
-        });
-      });
-    } catch (e) {
-      print(e);
-    }
+    //   await GallerySaver.saveImage(capturedFile.path).then((value) {
+    //     setState(() {
+    //       screenshotButtonText = 'screenshot saved!';
+    //     });
+    //   });
+    // } catch (e) {
+    //   print(e);
+    // }
   }
 }
